1296f001b846ea39360f8ca41a427df34ded4824
virak-mango-byte <virak@mango-byte.com>
2022-09-15 13:09:40 +0700

    chore: init

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from 'vite-plugin-eslint'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  buildModules: [
+    'nuxt-windicss'
+  ],
+  vite: {
+    plugins: [eslintPlugin()]
+  }
+})
---
+++ windi.config.ts
@@ -0,0 +1,16 @@
+
+import { defineConfig } from 'windicss/helpers'
+function range (size, startAt = 1) {
+  return Array.from(Array(size).keys()).map(i => i + startAt)
+}
+export default defineConfig({
+  preflight: false,
+  safelist: [
+    range(30).map(i => `p-${i}`), // p-1 to p-30
+    range(10).map(i => `mt-${i}`) // mt-1 to mt-10
+  ],
+  extract: {
+    include: ['**/*.{vue,html,jsx,tsx}'],
+    exclude: ['node_modules', '.git']
+  }
+})
