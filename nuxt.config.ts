de366183e7744959f44c63ee1f24ba8686d91381
virak-mango-byte <virak@mango-byte.com>
2022-09-15 13:34:50 +0700

    chore: add pina store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,16 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: [
-    'nuxt-windicss'
+    'nuxt-windicss',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          'defineStore',
+          ['defineStore', 'definePiniaStore']
+        ]
+      }
+    ]
   ],
   vite: {
     plugins: [eslintPlugin()]
