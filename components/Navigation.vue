712864ef063ea2e64b694562873d542072371dfd
virak-mango-byte <virak@mango-byte.com>
2022-09-30 15:57:28 +0700

    chore: improvement

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between mx-auto max-w-7xl py-4">
+  <div class="flex justify-between py-4 shadow shadow-lg rounded-lg md:px-5">
     <div class="flex items-center gap gap-4 ml-2 uppercase">
       <NuxtLink v-for="(menu, index) in menus" :key="index" :to="menu.link">
         {{ menu.name }}
@@ -21,14 +21,12 @@
           d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
         />
       </svg>
-      <div class="flex ml-3">
-        <img
-          class="h-8 w-8 rounded-full cursor-pointer"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        >
-        <CommonTheme class="ml-3" />
-      </div>
+      <img
+        class="h-8 w-8 rounded-full cursor-pointer"
+        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt=""
+      >
+      <CommonTheme class="ml-3" />
     </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 import { defineNuxtConfig } from 'nuxt'
 import eslintPlugin from 'vite-plugin-eslint'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: [
     'nuxt-windicss',
